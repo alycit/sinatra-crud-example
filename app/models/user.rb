69bf6ba787b4de92a9f6a03fbc91b09be9968767
Alyssa Diaz <alydiaz77@gmail.com>
2016-10-18 11:19:14 -0500

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,27 @@ class User < ActiveRecord::Base
   has_many :games
 
   validates :username, :name, presence: true
+  validate :validate_password
 
   def password
     @password ||= Password.new(password_type_thing)
   end
 
   def password=(plain_text_password)
+    @raw_password = plain_text_password
     @password = Password.create(plain_text_password)
     self.password_type_thing = @password
   end
 
   def authenticate(plain_text_password)
     self.password == plain_text_password
+  end
+
+  def validate_password
+    if @raw_password.nil?
+      errors.add(:password, "is required")
+    elsif @raw_password.length < 6
+      errors.add(:password, "must be 6 characters or more")
+    end
   end
 end
