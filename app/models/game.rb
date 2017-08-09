6aabba7edff0c9dc5faeb0e928eea7f2ee0ace38
Alyssa Diaz <alydiaz77@gmail.com>
2017-08-09 12:45:36 -0500

    Add description

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   validates :name, presence: true
+  validates :description, presence: true
 end
--- db/migrate/20160325133958_create_schema.rb
+++ db/migrate/20160325133958_create_schema.rb
@@ -2,7 +2,7 @@ class CreateSchema < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name, null: false
-      t.references :user
+      t.string :description, null: false
     end
   end
 end
